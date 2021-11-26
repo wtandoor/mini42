116c50040264e708ebf39ac2d95c136025375e5d
masharaven <74605798+masharaven@users.noreply.github.com>
2021-11-26 16:13:03 +0300

    Add files via upload
    hello

--- builtins/cd_updatepath.c
+++ builtins/cd_updatepath.c
@@ -56,64 +56,63 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return (s1[i] - s2[i]);
 }
 
-char	*ft_strdup(const char *s1)
-{
-	size_t	len;
-	char	*str;
-	char	*tmp;
+// char	*ft_strdup(const char *s1)
+// {
+// 	size_t	len;
+// 	char	*str;
+// 	char	*tmp;
 
-	len = ft_strlen(s1);
-	str = (char *)malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-		return (NULL);
-	tmp = str;
-	while (len--)
-		*str++ = *s1++;
-	*str = '\0';
-	return (tmp);
-}
+// 	len = ft_strlen(s1);
+// 	str = (char *)malloc(sizeof(char) * len + 1);
+// 	if (str == NULL)
+// 		return (NULL);
+// 	tmp = str;
+// 	while (len--)
+// 		*str++ = *s1++;
+// 	*str = '\0';
+// 	return (tmp);
+// }
 
 
-int	ft_strlen(const char *s)
-{
-	int i;
+// int	ft_strlen(const char *s)
+// {
+// 	int i;
 
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+// 	i = 0;
+// 	if (!s)
+// 		return (0);
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
 
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char			*dest;
-	unsigned int	i;
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	char			*dest;
+// 	unsigned int	i;
 
-	i = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	dest = (char*)malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (dest == NULL)
-		return (NULL);
-	while (*s1 != '\0')
-		dest[i++] = *s1++;
-	while (*s2 != '\0')
-		dest[i++] = *s2++;
-	dest[i] = '\0';
-	return (dest);
-}
+// 	i = 0;
+// 	if (s1 == NULL || s2 == NULL)
+// 		return (NULL);
+// 	dest = (char*)malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+// 	if (dest == NULL)
+// 		return (NULL);
+// 	while (*s1 != '\0')
+// 		dest[i++] = *s1++;
+// 	while (*s2 != '\0')
+// 		dest[i++] = *s2++;
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int	add_env(char *path, t_env *env)			//добавляет OLDPWD, если в предидущей он не нашелся				
-													//ЭТА ХУЕВАЯ ХЕРНЯ ВЫВОДИТ НЕВЕРНЫЙ ПУТЬ!!! ПЕРЕДЕЛАЙ
+int	add_env(char *path, t_env *env)			//добавляет OLDPWD, если в предидущей он не нашелся
 {
 	t_env *temp;
 	t_env *usable;
 
-	printf("patht: %s\n", path);	///ВОТ ТУТ ВЫВОДИТ      /Users/cc
+	printf("patht: %s\n", path);
 	if (env && env->value == NULL)
 	{
 		env->value = ft_strdup(path);
@@ -122,43 +121,21 @@ int	add_env(char *path, t_env *env)			//добавляет OLDPWD, если в �
 	usable = malloc(sizeof(t_env));
 	if (!usable)
 		return (-1);
-	usable->value = ft_strdup(path);
+	usable->value = ft_strdup(path); 
 	while (env && env->next && env->next->next)
+	{
+		printf("%s\n", env->value);
 		env = env->next;
+	}
+
 	temp = env->next;
 	env->next = usable;
+	// printf("temp: %s\n", temp->value);
+	// printf("after: env->next: %s\n", usable->value);
 	usable->next = temp;
-	
-	printf("старый env->next: %s\n", temp->value);
-	printf("новый env->next: %s\n", env->value);
-	// printf("новый env->next->next: %s\n", usable->next);
 	return (0);
-	// env->
 }
 
-int			env_add(const char *value, t_env *env) //в случае
-{
-	t_env	*new;								//ЭТА ХУЕВАЯ ХЕРНЯ ВЫВОДИТ НЕВЕРНЫЙ ПУТЬ!!! ПЕРЕДЕЛАЙ
-	t_env	*tmp;
-
-
-	printf("patht: %s\n", value); ///ВОТ ТУТ ВЫВОДИТ      /Users/cc
-
-	if (env && env->value == NULL)
-	{
-		env->value = ft_strdup(value);
-		return (SUCCESS);
-	}
-	if (!(new = malloc(sizeof(t_env))))
-		return (-1);
-	new->value = ft_strdup(value);
-	while (env && env->next && env->next->next)
-		env = env->next;
-	tmp = env->next;
-	env->next = new;
-	new->next = tmp;
-	return (SUCCESS);
-}
 
 
 
@@ -181,11 +158,11 @@ int update_old_path(t_env *env)
 	// printf(" old_path____: %s\n", old_path);
 	if (!old_path)
 		return (ERROR);
-	if (find_env(old_path, env) == 1) //если программа завершилась успешно, перед этим мы в oldpath добавили дирректорию, которая будет устаревшей при следующем  переходе
-		printf("patht: %s\n", old_path);
-		// env_add(old_path, env); //функция добавляет oldpwd сам если не нашел ее в исходнике////НО ОНА ВСТАВЛЯЕТ НЕВЕРНЫЙ ПУТЬ
+	if (find_env(old_path, env) == 0) //если программа завершилась успешно, перед этим мы в oldpath добавили дирректорию, которая будет устаревшей при следующем  переходе
+		add_env(old_path, env); //функция добавляет oldpwd сам если не нашел ее в исходнике////НО ОНА ВСТАВЛЯЕТ НЕВЕРНЫЙ ПУТЬ
 	// printf("напечатай мне что выведетfind_env: %i\n", find_env(old_path, env));
 	// printf("%s\n\n", old_path);
+	free_memo(old_path);
 	return (41);
 }
 
@@ -227,6 +204,23 @@ void	ft_lstadd_back_m(t_env **lst, t_env *new)
 	}
 }
 
+static char		*path_of_env(t_env *env, const char *var, size_t len) //функция ищет путь, который мы задаем. потом она его пихает в какую-то переменную
+{
+	char *pizdec;
+	int	i;
+	int lenght;
+
+	while (env && env->next)
+	{
+		if (ft_strncmp(var, env->value, len));
+		{
+			pizdec = malloc(sizeof (char *) (lenght - len));
+		}
+		env = env->next;
+	}
+	return (NULL);
+}
+
 
 int main(int argc, char **argv, char **env)
 {
@@ -247,6 +241,11 @@ int main(int argc, char **argv, char **env)
     // while (env1)
         // printf("%s\n", env1->value), env1 = env1->next;
 	update_old_path(env1);
-    
-    return 0;
+    // while (env1 && env1->next)
+	// {
+	// 	printf("%s\n", env1->value);
+	// 	env1 = env1->next;
+	// }
+	path_of_env(env1, "HOME", 4); // хероботина определяет путь home
+    return (0);
 }
