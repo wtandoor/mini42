c5d6a8e98b9a46cc7670c70e6f405b87178a012f
Weasley Tandoori <wtandoor@ge-i4.kzn.21-school.ru>
2021-12-13 18:33:33 +0300

    parsing

--- parse/parse.c
+++ parse/parse.c
@@ -94,7 +94,7 @@ t_token *add_token(char *s, int *i)
     j = 0;
     a = ' ';
     token = (t_token *)malloc(sizeof(t_token));
-    token->str = malloc(sizeof(char) * next_a(s, i)); //next_alloc
+    token->str = malloc(sizeof(char) * next_a(s, i)); //next_alloc //comp
     if (!token || !token->str)
         return (NULL);
     while (s[*i] && (s[*i]!= '\'' || a != ' '))
@@ -115,6 +115,12 @@ t_token *add_token(char *s, int *i)
     return (token);
 }
 
+void skip_chars(char *str, int *i)
+{
+    while((str[*i] == ' ' || str[*i] == '\t') || (str[*i] == '\r' || str[*i] == '\v' || str[*i] == '\f'))
+        (*i)++;
+}
+
 t_token *tokens(char *str)
 {
     t_token *next;
@@ -125,23 +131,95 @@ t_token *tokens(char *str)
     i = 0;
     next = NULL;
     prev = NULL;
-    skip_chars(str, &i); //skip_space
+    skip_chars(str, &i); //skip_space // comp
     while (str[i])
     {
-        separator = skip_separator(str, i); //ignore_sep
-        next = add_token(str, &i); //next_token
+        separator = skip_separator(str, i); //ignore_sep //comp
+        next = add_token(str, &i); //next_token //comp
         next->prev = prev;
         if (prev)
             prev->next = prev;
         prev = next;
         init_args(next, separator); //type_arg // comp 
-        skip_chars(str, &i);
+        skip_chars(str, &i);//comp
     }
     if (next)
         next->next = NULL;
     while (next && next->prev)
         next = next->prev;
     return (next);
+}
+
+int skip_separator(char *str, int i)
+{
+    if (str[i] && str[i] == '\\' && str[i + 1] && str[i + 1] == ';')
+        return (1);
+    else if (str[i] && str[i] == '\\' && str[i + 1] && str[i + 1] == '|')
+        return (1);
+    else if (str[i] && str[i] == '\\' && str[i + 1] && str[i + 1] == '>')
+        return (1);
+    else if (str[i] && str[i] == '\\' && str[i + 1] && str[i + 1] == '>' && str[i + 2] && str[i + 2] == '>')
+        return (1);
+    return (0);
+}
+
+int is_separator(char *str, int i)
+{
+    if (i > 0 && str[i - 1] == '\\' && ft_strchr("><;|", str[i]))
+        return (0);
+    else if (ft_strchr("><;|", str[i]) && quote(str, i) == 0)
+        return (0);
+    else
+        return (0);
+}
+
+int valid_param(t_token *token)
+{
+    t_token *prev1;
+
+    if (!token || type_search(token, 1) || type_search(token, 2))
+    {
+        prev1 = prev(token, 0);
+        if (!prev1 || type_search(prev1, 7) || type_search(prev1, 6))
+            return (1);
+        return (0);
+    }
+    else
+        return (0);
+
+}
+
+void take_args(t_mini *mini)
+{
+    t_token *prev1;
+    t_token *token;
+
+    token = mini->start;
+    while (token)
+    {
+        prev1 = prev(token, 0);
+        if (type_search(token, 2) && is_type(prev1, "TAI"))
+        {
+            while(valid_param(prev1) == 0) //is_last_valid_arg
+                prev1 = prev1->prev;
+            token->prev->next = token->next;
+            if (token->next)
+                token->next->prev = token->prev;
+            token->prev = prev1;
+            if (prev1)
+                token->next = prev1->next;
+            else
+                token->next = mini->start;
+            if (!prev1)
+                prev1 = token;
+            prev1->next->prev = token;
+            if (!mini->start->prev)
+                prev1->next = token;
+            if (mini->start->prev)
+                mini->start = mini->start->prev;
+        }
+        token = token->next;
+    }
 }
 
 void parse(t_mini *mini)
@@ -169,7 +247,7 @@ void parse(t_mini *mini)
         str[0] = (char)(-str[0]);
     mini->start = tokens(str);
     delete_memmory(str);
-    take_args(mini);//dopisat'
+    take_args(mini);//dopisat' //squish args
     token = mini->start;
     while (token)
     {
@@ -177,4 +255,10 @@ void parse(t_mini *mini)
             type_arg(token, 0);//dopisat' ..init_args 
         token = token->next;
     }
+}
+
+
+char *discovering(char *param, t_env *env, int err)
+{
+    
 }
